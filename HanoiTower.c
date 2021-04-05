0b0de6df54ff1f2efe57ba62466cd599f39d0ff2
awakedragon1204 <awakedragon1204@users.noreply.github.com>
2021-04-06 06:38:14 +0800

    don't know what I am doing

---
+++ HanoiTower.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int move(int n); 
+
+int main() {
+    int x;
+    printf("input the height of Hanoi Tower: ");
+    scanf("%d",&x);
+    move(x);
+}
+int move(int n) {
+    if (n>=2) {
+    printf("put the %d disk from 'A' to 'B'",n-move(n-1));
+    printf("put the %d disk from 'A' to 'C'",move(n));
+    }
+    if (n>=2) return move(n-1);
+    if (n==1) return 1;
+}
+
